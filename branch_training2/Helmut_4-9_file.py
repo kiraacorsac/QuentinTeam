2de9e0edab1d432e41d03912293c606e6052c263
vm035e <drexicek@gmail.com>
2020-04-09 14:14:08 +0200

    Added Easter Bunny

--- branch_training2/Helmut_4-9_file.py
+++ branch_training2/Helmut_4-9_file.py
@@ -1,4 +1,11 @@
+class EasterBunny(Bunny):
+    ears=2
+    legs=2
+    eggs=24
+    name="Helmut"
+
 print ("Happy Easter")
 print ("Enjoy the day's off :-)")
+print ("Hello, my name is %s" %EasterBunny.name)
 
 print ("See you again next Tuesday")
